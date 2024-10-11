46ec5fca5b70ac31a5831ebe9edb654174113b01
Mohammad Mahdi Afshar <me@mamad.dev>
2024-10-11 16:20:52 +0330

    merge

---
+++ index.ts
@@ -0,0 +1,49 @@
+import { URLPattern } from "@cloudflare/urlpattern-polyfill";
+
+interface Env {
+  VCS_TYPE: string;
+  VCS_URL: string;
+}
+
+const template = `<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="go-import" content="{{host}} {{vcs}} {{url}}">
+</head>
+</html>`;
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url);
+    const vcs = env.VCS_TYPE || "git";
+    const vcsURL = new URL(env.VCS_URL || "https://github.com/reloadlife");
+
+    if (url.pathname === "/ping") {
+      return new Response("ok", { status: 200 });
+    }
+
+    if (request.method !== "GET") {
+      return new Response("Method Not Allowed", { status: 405 });
+    }
+
+    const redirectURL = new URL(vcsURL.origin + vcsURL.pathname + url.pathname);
+
+    if (url.searchParams.get("go-get") !== "1" || url.pathname.length < 2) {
+      return Response.redirect(redirectURL.toString(), 307);
+    }
+
+    const host = url.host + url.pathname;
+    const htmlContent = template
+      .replace("{{host}}", host)
+      .replace("{{vcs}}", vcs)
+      .replace("{{url}}", redirectURL.toString());
+
+    return new Response(htmlContent, {
+      headers: {
+        "Content-Type": "text/html",
+        "Cache-Control": "no-store",
+      },
+    });
+  },
+};
---
+++ readme.md
@@ -0,0 +1,7 @@
+# go import redirector
+
+> yes, as a developer shame on me that it's a typescript version, but it's much cheaper to run it on cloudflare workers.
+> there's also a go version of this project (used to be on production) but it's not maintained anymore.
+
+
+Bye :)
